610a6c51b82a1c7a54d370e3ac08e685e07ad58f
Dilshit <dilshikk@mail.ru>
2026-08-12 13:56:38 +0500

    chore: remove unused siteTexts table from Convex schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -8,12 +8,4 @@ export default defineSchema({
     email: v.optional(v.string()),
     role: v.optional(v.string()),
   }).index("by_token", ["tokenIdentifier"]),
-
-  siteTexts: defineTable({
-    key: v.string(),
-    locale: v.string(),
-    value: v.string(),
-  })
-    .index("by_key_and_locale", ["key", "locale"])
-    .index("by_locale", ["locale"]),
 });
